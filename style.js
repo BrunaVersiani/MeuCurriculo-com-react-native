8bae124bbbc5b5570d3e226e113c6c9d170c8405
Bruna Versiani <76855528+BrunaVersiani@users.noreply.github.com>
2022-01-02 14:27:29 -0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,29 @@
+import {StyleSheet} from 'react-native';
+
+const style = StyleSheet.create({
+  
+  container: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10
+  },
+  card: {
+    width:'70%',
+    fontWeight: 'bold',
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: (`#ffb6c1`),
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 5,
+    backgroundColor: '#FFFAFA',
+    color: (`#dc143c`)
+  }, 
+  card_content: {
+    marginTop: 20,
+  }
+
+})
+
+export default style;
